80db268e5077a9710894c37165f23a857c1b7513
Lee Hanbi <hanbi6717@naver.com>
2026-01-28 09:42:13 +0900

    Lv2 18번 DATETIME에서 DATE로 형 변환

---
+++ src/main/java/com/example/sqlcodkata/SqlCodKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlcodkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlCodKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlCodKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sqlcodkata/SqlCodKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlcodkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlCodKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
